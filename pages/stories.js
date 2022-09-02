736eac49be1b85cb17f81a05578fca5a9718ad58
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-09-02 15:16:12 -0600

    feat: add async function for fetching stories from hackernews based of route.path

--- pages/stories.js
+++ pages/stories.js
@@ -1,6 +1,27 @@
+// https://node-hnapi.herokuapp.com
+
+// / (Top) -> /new
+// /new (New) -> /newest
+// /ask (Ask) -> /ask
+// /show (Show) -> /show 
+// const BASE_URL = 'https://node-hnapi.herokuapp.com'
 import view from '../utils/view.js';
 
-export default function Stories(path) {
+export default async function Stories(path) {
+  const stories = await getStories(path);
+  console.log(stories);
   view.innerHTML = `<div>${path}</div>`;
 }
 
+async function getStories(path) {
+  const isHomeRoute = path === '/';
+  const isNewRoute = path === '/new';
+  if (isHomeRoute) {
+    path = '/news';
+  } else if (isNewRoute) {
+    path = '/newest';
+  }
+  const response = await fetch(`https://node-hnapi.herokuapp.com${path}`);
+  const stories = await response.json();
+  return stories; 
+}
