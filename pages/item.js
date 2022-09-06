468fe9f3d1d3faa6c29aef383d07c4d948d7da25
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-09-06 10:15:02 -0600

    feat: add error handling for bad story id in URI

--- pages/item.js
+++ pages/item.js
@@ -2,16 +2,29 @@ import Story from '../components/Story.js';
 import view from '../utils/view.js'
 
 export default async function Item() {
-  const story = await getStory();
-  const hasComments = story.comments.length > 0;
+  let story = null;
+  let hasComments = false;
+  let hasError = false; //piece of local story
+  // block scoping
+  try { 
+    story = await getStory();
+    hasComments = story.comments.length > 0;
+  } catch (error) {
+    hasError = true;
+    console.log(error);
+  }
 
-  view.innerHTML = `
-    <div>
-      ${Story(story)}
-    </div>
-    <hr/>
-    ${hasComments ? story.comments.map(comment => JSON.stringify(comment)).join('') : 'No Comments'}
-  `
+  if (hasError) {
+    view.innerHTML = `<div class="error">No Story Found</div>`
+  } else {
+    view.innerHTML = `
+      <div>
+        ${Story(story)}
+      </div>
+      <hr/>
+      ${hasComments ? story.comments.map(comment => JSON.stringify(comment)).join('') : 'No Comments'}
+      `
+  }
 }
 
 async function getStory() {
